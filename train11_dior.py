a9cc5ce78793f95d1479f3567c0d7c28cca9d80f
meijinlong <70095066+meijinlong@users.noreply.github.com>
2025-10-13 20:36:07 +0800

    Add files via upload

---
+++ train11_dior.py
@@ -0,0 +1,16 @@
+from ultralytics import YOLO
+
+# 加载一个预训练的 YOLO11n 模型
+model = YOLO("/home/lwt/mjl/YOLO/ultralytics-main1/yolo11n.yaml").load("/home/lwt/mjl/YOLO/ultralytics-main1/yolo11n.pt")
+
+# 在 crowdhuman 数据集上训练模型 100 个周期
+train_results = model.train(
+    data="dior.yaml",  # 数据集配置文件路径
+    epochs=300,  # 训练周期数
+    imgsz=800,  # 训练图像尺寸
+    batch=8,   #batch_size
+    device=0,  # 运行设备（例如 'cpu', 0, [0,1,2,3]）
+)
+
+## 评估模型在验证集上的性能
+# metrics = model.val()
